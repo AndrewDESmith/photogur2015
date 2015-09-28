dc443ca013b0c31bffc3029ea39ab6c637fb6fad
Andrew Smith <andrewsmith@Andrews-iMac.local>
2015-09-28 17:44:40 -0400

    Adding features to create new picture using HTML form

--- photogur2015/config/routes.rb
+++ photogur2015/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
 
+  post 'pictures' => 'pictures#create'
+  get 'pictures/new' => 'pictures#new'
+
   get 'pictures/:id' => 'pictures#show', as: "picture"
   # as: "picture" allows us to refer to this route as picture_path (the string is prepended to _path).
 end
