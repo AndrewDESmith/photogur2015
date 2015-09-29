d7dd22c618e794bdec6aab0060f57951af609e99
Andrew Smith <andrewsmith@Andrews-iMac.local>
2015-09-29 14:24:47 -0400

    Refactoring to save new pictures in database

--- photogur2015/app/controllers/pictures_controller.rb
+++ photogur2015/app/controllers/pictures_controller.rb
@@ -8,10 +8,27 @@ class PicturesController < ApplicationController
   end
 
   def new
-
+    @picture = Picture.new
+    # new only creates the local object but does not attempt to validate or save it to the DB.  Here, the local object is empty, since the user hasn't yet filled in the form on the new page with the picture's attributes.
+    # An HTTP GET to /resources/new is intended to render a form suitable for creating a new resource, which it does by calling the new action within the controller, which creates a new unsaved record and renders the form.
   end
 
   def create
-    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+    @picture = Picture.new(picture_params)
+    # create instantiates the new object, validates it, and then saves it to the database.
+    # An HTTP POST to /resources takes the record created as part of the new action and passes it to the create action within the controller, which then attempts to save it to the database.
+    if @picture.save
+      redirect_to pictures_url
+      # If the save for the picture was successful, go to index.html.erb (where the newly created picture will be visible as the last entry in the index of pictures).
+    else
+      render :new
+      # Otherwise render the view associated with the action :new (i.e. new.html.erb)
+      # If the picture could not be saved, return the user to the new picture form (render the page again) to try again (most likely, validations didn't pass due to user behavior).
+    end
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
   end
 end
