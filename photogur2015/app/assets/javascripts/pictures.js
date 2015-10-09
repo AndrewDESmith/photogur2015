059f53f9f0213b0740e8932fe28193fd28abdaab
Andrew Smith <andrewsmith@Andrews-iMac.local>
2015-10-09 11:57:37 -0400

    Got the basic jQuery slider working.

--- photogur2015/app/assets/javascripts/pictures.js
+++ photogur2015/app/assets/javascripts/pictures.js
@@ -1,8 +1,29 @@
 $(document).ready(function() {
-  var t = setInterval(function() {
-      $("ul").animate({marginLeft:-480}, 1000, function() {
-        $(this).find("li:last").after($(this).find("li:first"));
-        $(this).css({marginLeft: 0});
-      })
-  }, 3000);
+
+    var $slider = $('.slider');
+    var $slide = 'li';
+    var $slides = $slider.find($slide);
+    var fadeInTime = 1500;
+    var fadeOutTime = 1500;
+    var time_between_slides = 4000;
+
+    $slides.first().addClass('active');
+    $slides.first().fadeIn(fadeInTime);
+
+    setInterval(
+      function() {
+        var $i = $slider.find($slide + '.active').index();
+
+        $slides.eq($i).removeClass('active');
+        $slides.eq($i).fadeOut(fadeOutTime);
+
+        if ($slides.length == $i + 1)
+          $i = -1;
+
+        $slides.eq($i + 1).fadeIn(fadeInTime);
+        $slides.eq($i + 1).addClass('active');
+      }
+      , time_between_slides
+    );
+
 });
